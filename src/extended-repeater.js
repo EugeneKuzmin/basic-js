77ab20432df432363f9ee37326a2bc5c18b8ed00
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-12-23 20:56:53 +0300

    feat:9 tasks finish

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,21 +16,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  console.log('str',str)
-  console.log('options',options)
-  let str1 = ''
-  let separator = options.separator?options.separator:'+'
+  const separator = options.separator?options.separator:'+'
   
-  if(options.addition){
-    str = (String(options.addition)+String(options.additionSeparator)).repeat(options.additionRepeatTimes)
-    return (str+separator).repeat(options.repeatTimes)
-  }else{
-    str1 = '+'
-    let strres = (str+separator).repeat(options.repeatTimes)
-    return strres.slice(0,strres.length - 1)
+  var strres = ''
+  
+  if(options.hasOwnProperty('addition')){
+    const additionSeparator = options.additionSeparator?options.additionSeparator:
+    options.additionRepeatTimes?'|':''
+    if(options.additionRepeatTimes){
+      strres = (String(options.addition)+additionSeparator).repeat(options.additionRepeatTimes)
+      strres = strres.slice(0,strres.length - additionSeparator.length)
+    }else{
+      strres = String(options.addition)
+    }
   }
-  
-  
+
+  if(options.repeatTimes){
+    strres = (str+ strres + separator).repeat(options.repeatTimes)
+    strres = strres.slice(0,strres.length - separator.length)
+  }else{
+    strres = str+ strres
+  }
+   return strres
 }
 
 module.exports = {
