b0dd4b895d28868433875bc70b0a592ee3aa0333
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-12-25 22:11:04 +0300

    feat:16 tasks finish

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  const resSumm = String(n).split('').reduce((sum,l)=>{
+    sum += Number(l);
+    return sum
+  },0)
+  if(resSumm.toString().length>1){
+    return getSumOfDigits(resSumm)
+  }else{
+    return resSumm
+  }
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr)){
+    const arrFilters = ['--discard-prev','--discard-next','--double-next','--double-prev']
+    const arrProcessed = arr.filter(x=> arrFilters.includes(x))
+    arrProcessed.forEach(l=>{
+      if(l === '--discard-prev'){
+        let dscrdPrevPosition = arr.indexOf('--discard-prev')
+        if(dscrdPrevPosition !== 0){
+          arr.splice(dscrdPrevPosition - 1,1)
+        }
+      }
+      if(l === '--discard-next'){
+        let dscrdNextPosition = arr.indexOf('--discard-next')
+        if(dscrdNextPosition !== (arr.length - 1)){
+          arr.splice(dscrdNextPosition+1,1)
+        }
+      }
+      if(l === '--double-next'){
+        let dblNextPosition = arr.indexOf('--double-next')
+        if(dblNextPosition !== (arr.length - 1)){
+          const l = arr[dblNextPosition]
+          arr.splice(dblNextPosition+1,0,l)
+        }
+      }
+      if(l === '--double-prev'){
+        let dblPrevPosition = arr.indexOf('--double-prev')
+        if(dblPrevPosition !== 0){
+          const l = arr[dblPrevPosition]
+          arr.splice(dblPrevPosition - 1,0,l)
+        }
+      }
+    })
+    arr = arr.filter(x=>x !=='--discard-prev').filter(x=>(x !=='--discard-next')).filter(x=>(x !=='--double-next')).filter(x=>(x !=='--double-prev'))
+    return arr;
+  }else{
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
 }
 
 module.exports = {
